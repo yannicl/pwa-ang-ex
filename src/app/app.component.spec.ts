6bc33d6adbf80413fa8ad7035541849e54b3eb78
Yannic Luyckx <yannic.luyckx@gmail.com>
2022-02-05 21:04:26 -0500

    fix unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'app-ex'`, () => {
+  it(`should have as title 'Mes tâches'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('app-ex');
+    expect(app.title).toEqual('Mes tâches');
   });
 
-  it('should render title', () => {
+  it('should render toolbar', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('app-ex app is running!');
+    expect(compiled.querySelector('mat-toolbar span')?.textContent).toContain('Mes tâches');
   });
 });
