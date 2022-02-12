4e810f709653b1957c8686b6dad331d3be0cb5ee
Yannic Luyckx <yannic.luyckx@gmail.com>
2022-02-12 15:53:10 -0500

    ajout d'une route avec un parametre de requete

--- src/app/components/b/b.component.ts
+++ src/app/components/b/b.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-b',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BComponent implements OnInit {
 
-  constructor() { }
+  name: string = "";
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.name = params['name'];
+    });
   }
 
 }
